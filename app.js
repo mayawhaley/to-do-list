a240fb17c74847f8897ad01ec697a39782a05551
Maya Whaley <mawhaley430@gmail.com>
2020-11-08 18:44:53 -0500

    add functionality to button

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // app.js
+
+let addButton = document.querySelector('#addText')
+let toDoContainer = document.querySelector('#toDoContainer')
+let inputField = document.querySelector('#inputField')
+
+// creates new element as paragraph when button is pushed
+addButton.addEventListener('click', function () {
+    var paragraph = document.createElement('p');
+    paragraph.innerText = inputField.value;
+    paragraph.classList.add('paragraph-style')
+    // append new element to container 
+    toDoContainer.appendChild(paragraph);
+    inputField.value = ' ';
+
+    paragraph.addEventListener('click', function () {
+        paragraph.style.textDecoration = "line-through";
+        toDoContainer.removeChild(toDoContainer)
+    })
+
+
+    paragraph.addEventListener('dblclick', function () {
+        toDoContainer.removeChild(paragraph)
+    })
+})
